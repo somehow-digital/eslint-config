be04ff87035eab1e1833615b8a1a3754a622b0cf
Thomas Deinhamer <code@thasmo.dev>
2024-05-08 21:14:27 +0200

    Update config files

--- configuration/basic.js
+++ configuration/basic.js
@@ -1,9 +1,9 @@
-import eslint from '@eslint/js';
+import javascript from '@eslint/js';
 import unicorn from 'eslint-plugin-unicorn';
 import prettier from 'eslint-plugin-prettier/recommended';
 
 export default [
-	eslint.configs.recommended,
+	javascript.configs.recommended,
 	unicorn.configs['flat/recommended'],
 	prettier,
 ];
--- configuration/typescript.js
+++ configuration/typescript.js
@@ -1,11 +1,11 @@
-import eslint from '@eslint/js';
-import typescript from '@typescript-eslint/eslint-plugin';
+import javascript from '@eslint/js';
+import typescript from 'typescript-eslint';
 import unicorn from 'eslint-plugin-unicorn';
 import prettier from 'eslint-plugin-prettier/recommended';
 
 export default [
-	eslint.configs.recommended,
-	typescript.configs.recommended,
+	javascript.configs.recommended,
+	...typescript.configs.strict,
 	unicorn.configs['flat/recommended'],
 	prettier,
 ];
