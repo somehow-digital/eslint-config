fa501518e53544d32b770d43d6e1b01d5e675214
Thomas Deinhamer <code@thasmo.dev>
2024-05-08 23:22:26 +0200

    Update cspell command

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 export default {
 	'**/*.{js,jsx,ts,tsx}': ['eslint'],
 	'**/*.{md,json,yml,yaml}': ['prettier --check'],
-	'**/*.*': ['cspell --no-progress'],
+	'**/*.*': ['cspell --no-progress --no-must-find-files'],
 };
