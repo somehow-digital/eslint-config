23d565a786c2b12a3963623d7364378f4d49611a
Thomas Deinhamer <184284+thasmo@users.noreply.github.com>
2025-07-17 20:48:23 +0200

    fix: use JavaScript instead of TypeScript (#432)

--- configuration/index.js
+++ configuration/index.js
@@ -1,7 +1,7 @@
 import antfu from '@antfu/eslint-config';
 import defaults from './defaults.js';
 
-export default function config(options?, ...configurations) {
+export default function config(options, ...configurations) {
 	return antfu({
 		...defaults,
 		...options,
